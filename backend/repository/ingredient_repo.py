f03e22e87b203b605b7eb45b880966cfb0763885
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-04-30 13:13:15 -0300

    #12 - fix: validation

--- backend/repository/ingredient_repo.py
+++ backend/repository/ingredient_repo.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from sqlalchemy import select
 import service.ingredient_service as service
 import models.ingredient_model as model
 import schemas.ingredient_schema as schema
@@ -13,6 +13,8 @@ def create_ingredient(ingredient: schema.IngredientCreate, db: Session = Depends
     new_ingredient = service.object_conversion(ingredient)
     if not new_ingredient : 
         raise HTTPException(status_code=422, detail="The conversion has failed")
+    if db.execute(select(model.Ingredient).where(model.Ingredient.name == new_ingredient.name)) :
+        raise HTTPException(status_code=409, detail="This ingredient alredy exists...")
     db.add(new_ingredient)
     db.commit()
     db.refresh(new_ingredient)
@@ -24,4 +26,12 @@ def get_ingredient_id(id: int, db: Session = Depends(database.get_db)) :
     if not found_ingredient :
         raise HTTPException(status_code=404, detail="We cannot find the ingredient!")
     return found_ingredient
-    
+
+@router.get("/{name}")
+def get_ingredient_name(name: str, db: Session = Depends(database.get_db)) :
+    found_ingredient = db.execute(select(model.Ingredient).where(model.Ingredient.name == name))
+    if not found_ingredient :
+        raise HTTPException(status_code=404, detail="We cannot find the ingredient")
+    if db.execute(select(model.Ingredient).where(model.Ingredient.name == found_ingredient.name)) :
+        raise HTTPException(status_code=409, detail="Have two or more identical objects in system...")
+    return found_ingredient
