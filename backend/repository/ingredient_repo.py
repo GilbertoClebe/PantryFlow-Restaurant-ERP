9d25602471e73c2336f2235842289feeec3afcb7
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-04-30 12:36:47 -0300

    #11 - feat: get_ingredient_by_id done

--- backend/repository/ingredient_repo.py
+++ backend/repository/ingredient_repo.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import service.ingredient_service as service
-import database
+import models.ingredient_model as model
 import schemas.ingredient_schema as schema
+import database
 router = APIRouter()
 
-@router.post("/create", response_model=schema.IngredientResponse)
+@router.post("/", response_model=schema.IngredientResponse)
 def create_ingredient(ingredient: schema.IngredientCreate, db: Session = Depends(database.get_db)) :
 
     new_ingredient = service.object_conversion(ingredient)
@@ -17,5 +18,10 @@ def create_ingredient(ingredient: schema.IngredientCreate, db: Session = Depends
     db.refresh(new_ingredient)
     return new_ingredient
 
-
+@router.get("/{id}")
+def get_ingredient_id(id: int, db: Session = Depends(database.get_db)) :
+    found_ingredient = db.get(model.Ingredient, id)
+    if not found_ingredient :
+        raise HTTPException(status_code=404, detail="We cannot find the ingredient!")
+    return found_ingredient
     
